3204c3a51af054b26ab7fbcc7cd223c67d7e3bf6
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-08-10 19:35:34 +0430

    Load web page direction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { I18nService } from './i18n/i18n.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  dir = 'ltr';
+  constructor(private i18n: I18nService) {}
+  ngOnInit() {
+    this.i18n.getLang().subscribe(ok => {
+      this.dir = ok.dir;
+    }, err => {
+      console.log(err);
 
+    });
+  }
 }
