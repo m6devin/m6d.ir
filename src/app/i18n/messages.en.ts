88eb8b2bc3ac3d5fc22b11e0df0dfec019893018
Mohammad Gholami <gholami.mohammad.mgh@gmail.com>
2018-08-10 21:16:31 +0430

    add translation of navbar

--- src/app/i18n/messages.en.ts
+++ src/app/i18n/messages.en.ts
@@ -2,4 +2,10 @@ export let messages = {
   'welcome': 'Welcome to my website!',
   'nice_meet': 'It\'s Nice To Meet You',
   'mgh': 'Mohammad Gholami',
+  'services': 'Services',
+  'portfolio': 'Portfolio',
+  'about': 'About',
+  'our_team': 'Team',
+  'out_clients': 'Our Clients',
+  'contact': 'Contact',
 };
--- src/app/i18n/messages.fa.ts
+++ src/app/i18n/messages.fa.ts
@@ -1,5 +1,11 @@
 export let messages = {
   'welcome': 'به وب سایت من خوش آمدید!',
   'nice_meet': 'از دیدار شما خوش وقتم.',
-  'mgh': 'محمد غلامی'
+  'mgh': 'محمد غلامی',
+  'services': 'خدمات',
+  'portfolio': 'نمونه کار',
+  'about': 'درباره',
+  'our_team': 'تیم ما',
+  'out_clients': 'مشتریان',
+  'contact': 'تماس باما',
 };
